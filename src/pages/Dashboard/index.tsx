3bb951f323893503825e635808ddc8ef4ee13aa4
Denys Lonkovski Maiolli <denysmaiolli@hotmail.com>
2021-03-01 18:00:13 -0300

    add: dashboard style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/69089729?s=460&u=cc306cce7dfffcdb91853bb23f49ee95996adb85&v=4"
+            alt="Denys Lonkovski"
+          />
+          <div>
+            <strong>dmaiolli/github-explorer</strong>
+            <p>Repositório para listar projetos de uma pessoa do github</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
